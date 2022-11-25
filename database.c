a1a75e24f8ca95d875ed4c6b7c5a538f8a613937
AFNAN6205 <111301704+AFNAN6205@users.noreply.github.com>
2022-11-25 19:37:26 +0530

    Add files via upload

--- database.c
+++ database.c
@@ -11,13 +11,17 @@ new[j] = '\0';
 sscanf(new,"%d",&x);
 return x;
 }
+
 typedef struct student{
     int rollNo;
+    float cgpa;
     char hallticketno[15];
     char name[100];
     char fatherName[100];
     char address[100];
     int marks[4];
+    float percentage;
+    long int mobileNo;
 } stu;
 
 
@@ -26,7 +30,6 @@ int main(){
     int n,ans;
     printf("How many students are there:");
     scanf("%d",&n);
-    float cgpa[n];
     stu st[n];
 
     for(int i=0; i<n; i++){
@@ -38,37 +41,42 @@ int main(){
         scanf("%s",&st[i].fatherName);
         printf("Enter your address:");  
         scanf("%s",&st[i].address);
+        printf("Enter your mobile no:");  
+        scanf("%ld",&st[i].mobileNo);
         st[i].rollNo = slice(st[i].hallticketno, 8, 11);
     } 
      printf("Do you want to enter marks?\n press 1 to enter");  
      scanf("%d",&ans);
      if(ans==1){
         for(int i=0; i<n; i++){
-            for(int j=0; j<1; j++){
             
-            printf("Enter marks of mathamatics:");
-            scanf("%d",&st[i].marks[j]);
-            printf("Enter marks of physics:");
-            scanf("%d",&st[i].marks[j+1]);
-            printf("Enter marks of pps:");
-            scanf("%d",&st[i].marks[j+2]);
-            printf("Enter marks of eg:");
-            scanf("%d",&st[i].marks[j+3]);
-            }
-            cgpa[i] = ((st[i].marks[1]+st[i].marks[2]+st[i].marks[3]+st[i].marks[4])/400) * 100;
+            printf("Enter marks of mathamatics for student %d:",i+1);
+            scanf("%d",&st[i].marks[0]);
+            printf("Enter marks of physics for student %d:",i+1);
+            scanf("%d",&st[i].marks[1]);
+            printf("Enter marks of pps for student %d:",i+1);
+            scanf("%d",&st[i].marks[2]);
+            printf("Enter marks of eg for student %d:",i+1);
+            scanf("%d",&st[i].marks[3]);
+            st[i].percentage = st[i].marks[0]+st[i].marks[1]+st[i].marks[2]+st[i].marks[3];
         }
      }
      else{
         exit(0);
      }
+        
+    
+     
     for(int i=0; i<n; i++){
         printf("\n\n\n\nName: %s\n",st[i].name);
         printf("Hallticket no.: %s\n",st[i].hallticketno);
         printf("Roll no: %d\n",st[i].rollNo);
         printf("Father name: %s\n",st[i].fatherName);
         printf("Address: %s\n",st[i].address);
+         printf("Mobile no.:%d\n",st[i].mobileNo);  
         if(ans==1){
-         printf("Obtained marks: %.2f",cgpa[i]);  
+            st[i].percentage /= 400;
+         printf("Obtained marks: %.2f",st[i].percentage*100);  
         }
      } 
 
